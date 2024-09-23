9809562c011baae38abbcb2cd6a2dce9a8e7e2a0
AdrienGoups <agoupil2005@gmail.com>
2024-09-23 13:00:07 -0400

    Initial commit

---
+++ src/scripts/Lottie.js
@@ -0,0 +1,12 @@
+export default class Lottie {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    // Votre code pourrait commencer comme ceci
+    // const animation = lottieWeb.loadAnimation({
+    //   ..
+    // });
+  }
+}
